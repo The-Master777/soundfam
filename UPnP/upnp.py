fd15b2e1627298d7eea2540f0779fde7106e1a27
The-Master777 <themaster777@outlook.com>
2015-05-24 18:11:19 +0200

    Basic UPnP service discovery

---
+++ UPnP/upnp.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import ssdp
+
+class UPnPMediaRenderer(object):
+	_ST_DEVICE_MEDIA_RENDERER = 'urn:schemas-upnp-org:device:MediaRenderer:1'
+
+	def __init__(self, arg):
+		super(UPnPMediaRenderer, self).__init__()
+		self.arg = arg
+
+	@staticmethod
+	def discover():
+		rs = ssdp.BasicSSDPServiceDiscoverer().discover(UPnPMediaRenderer._ST_DEVICE_MEDIA_RENDERER)
+
+		if rs is not None:
+			for r in rs: print(r)
+
+
+if __name__ == '__main__':
+	UPnPMediaRenderer.discover()
