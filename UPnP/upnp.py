a2496682dba5eb1df3f66a6eee5594f1790e1044
The-Master777 <themaster777@outlook.com>
2015-06-07 18:00:47 +0200

    basic Upnp device code

--- UPnP/upnp.py
+++ UPnP/upnp.py
@@ -3,20 +3,62 @@
 
 import ssdp
 
-class UPnPMediaRenderer(object):
-    _ST_DEVICE_MEDIA_RENDERER = 'urn:schemas-upnp-org:device:MediaRenderer:1'
+class UPnPDevice(object):
+    # Value of the SSDP discovery ST parameter
+    _DISCOVER_ST = ssdp.SSDPServiceDiscoverer.ST_VALUES['rootdevice']
 
-    def __init__(self, arg):
-        super(UPnPMediaRenderer, self).__init__()
-        self.arg = arg
+    def __init__(self, ssdpResponse):
+        super(UPnPDevice, self).__init__()
+        
+        self.ssdpResponse = ssdpResponse
+
+    def __repr__(self):
+        return '<UPnPDevice from %s>' % self.ssdpResponse.__repr__()
 
     @staticmethod
-    def discover():
-        rs = ssdp.BasicSSDPServiceDiscoverer().discover(UPnPMediaRenderer._ST_DEVICE_MEDIA_RENDERER)
+    def _yieldDevices(c, rs, **kwargs):
+        if rs is None:
+            return
 
-        if rs is not None:
-            for r in rs: print(r)
+        for r in rs:
+            # Create new instance
+            yield c(r, **kwargs)
 
+    @classmethod
+    def discover(c, st=None, **kwargs):
+        """Search for UPnP devices and yield encapsulated device responses."""
+
+        st = st or c._DISCOVER_ST
+        rs = ssdp.BasicSSDPServiceDiscoverer().discover(st)
+
+        #yield from UPnPDevice._yieldDevices(c, rs, **kwargs)
+        for bar in UPnPDevice._yieldDevices(c, rs, **kwargs):
+            yield bar
+
+    @classmethod
+    def fromUuid(c, deviceId, **kwargs):
+        rs = ssdp.BasicSSDPServiceDiscoverer().discoverDevice(deviceId)
+
+        #yield from UPnPDevice._yieldDevices(c, rs, **kwargs)
+        for bar in UPnPDevice._yieldDevices(c, rs, **kwargs):
+            return bar # Return FIRST result, as the uuid is assumed to be unique
+
+        return None
+
+
+class UPnPMediaRenderer(UPnPDevice):
+    # Service Type of UPnP MediaRenderers
+    _ST_DEVICE_MEDIA_RENDERER = 'urn:schemas-upnp-org:device:MediaRenderer:1'
+
+    # Value of the SSDP discovery ST parameter
+    _DISCOVER_ST = _ST_DEVICE_MEDIA_RENDERER
+
+    def __init__(self, ssdpResponse):
+        super(UPnPMediaRenderer, self).__init__(ssdpResponse)
+
+#class SonosZonePlayer(UPnPMediaRenderer):
+#    _DISCOVER_ST = 'urn:schemas-upnp-org:device:ZonePlayer:1'
 
 if __name__ == '__main__':
-    UPnPMediaRenderer.discover()
+    for s in UPnPMediaRenderer.discover():
+        print(s)
